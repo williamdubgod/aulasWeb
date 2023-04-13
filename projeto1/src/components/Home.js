5525d4ede9d6f62a913bd8e9b7df99b8f531c540
williamdubgod <william_vulcano@hotmail.com>
2023-04-13 20:55:59 -0300

    aula useState

--- projeto1/src/components/Home.js
+++ projeto1/src/components/Home.js
@@ -1,9 +1,48 @@
 import './css/style.css'
+import React, {useState} from 'react'
 
 export default function Home() {
+    //criando o useState (manipulação o estado da variavel)
+
+    const[nome, setNome] = useState('Will')
+    const[email, setEmail] = useState()
+    const[userEmail, setUserEmail] = useState()
+
+    //Criando função para enviar email
+
+    function enviarEmail(e) {
+        e.preventDefault()
+
+        setUserEmail(email)
+        console.log(userEmail);
+    }
+
+    //Criando função para limpar o campo de email
+
+    function limparCampo() {
+        setUserEmail('')
+    }
+
     return (
         <div className="home">
         <h1>Home</h1>
+        <br />
+        <p className='nome'>Nome: {nome}</p>
+        <br />
+        <button onClick={()=> setNome('Vulcano')}>Trocar</button>
+        <br /><br />
+        <h2>Cadastre seu E-Mail</h2>
+        <input type='email' placeholder='Digite seu E-Mail' onChange={(e)=> setEmail(e.target.value)}/> 
+        <button type='submit' onClick={enviarEmail}>Enviar</button>
+        <br /><br />
+        {/* {email} */}
+        {/* Condicional para ver se o email esta preenhido */}
+        {userEmail && (
+            <div>
+                <p id='userEmail'>O email do usuario é: <span>{userEmail}</span></p> <br />
+                <button onClick={limparCampo}>Limpar</button>
+            </div>
+        )}
         </div>
     )
 }
